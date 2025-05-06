1851246347d2e843a49ff4bfbcad2d760b1ef192
lammarboudjelal <linammarboudjelal@gmail.com>
2025-05-06 18:43:36 +0200

    Correspondance à la maquette de la page de connexion

--- gestionNotes/src/routes/Connexion.jsx
+++ gestionNotes/src/routes/Connexion.jsx
@@ -5,6 +5,7 @@ import {
   Button,
   FormControl,
   FormControlLabel,
+  Grid,
   Radio,
   RadioGroup,
   TextField,
@@ -35,69 +36,94 @@ const Connexion = () => {
     };
 
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            rowGap="2rem"
-            padding="5rem"
-            minHeight="100vh"
-        >
-            <Typography variant="h4">
-                Bienvenue !
-            </Typography>
-
-            <Box 
-                component="form" 
-                onSubmit={handleSubmit} 
-                display="flex" 
-                flexDirection="column" 
-                rowGap="1rem"
+        <Grid container>
+            <Grid 
+                size={6} 
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                rowGap="10vh"
+                minHeight="100vh" 
+                bgcolor="#181C3C"
             >
-                <FormControl>
-                    <RadioGroup
-                        row
-                        value={typeUtilisateur}
-                        onChange={(e) => setTypeUtilisateur(e.target.value)}
-                    >
-                        <FormControlLabel
-                            value="etudiant"
-                            control={<Radio />}
-                            label="Étudiant"
-                        />
-                        
-                        <FormControlLabel
-                            value="enseignant"
-                            control={<Radio />}
-                            label="Enseignant"
-                        />
-                    </RadioGroup>
-                </FormControl>
+                <Typography variant="h2" fontWeight="bold" color="white">
+                    Noty
+                </Typography>
 
-                <TextField
-                    label="Identifiant"
-                    variant="outlined"
-                    fullWidth
-                    value={identifiant}
-                    onChange={(e) => setIdentifiant(e.target.value)}
-                    required
-                />
+                <img src="../../public/noty.png" style={{width:"25vw"}}/>
+            </Grid>
 
-                <TextField
-                    label="Mot de passe"
-                    variant="outlined"
-                    type="password"
-                    fullWidth
-                    value={motDePasse}
-                    onChange={(e) => setMotDePasse(e.target.value)}
-                    required
-                />
+            {/* Formulaire de connexion */}
+            <Grid 
+                size={6}
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                rowGap="2rem"
+                padding="5vw"
+                minHeight="100vh"
+            >
+                <Typography variant="h4">
+                    Bienvenue !
+                </Typography>
 
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                    Connexion
-                </Button>
-            </Box>
-        </Box>
+                <Box 
+                    component="form" 
+                    onSubmit={handleSubmit} 
+                    display="flex" 
+                    flexDirection="column" 
+                    rowGap="1rem"
+                >
+                    {/* Type d'utilisateur */}
+                    <FormControl>
+                        <RadioGroup
+                            row
+                            value={typeUtilisateur}
+                            onChange={(e) => setTypeUtilisateur(e.target.value)}
+                        >
+                            <FormControlLabel
+                                value="etudiant"
+                                control={<Radio />}
+                                label="Étudiant"
+                            />
+                            
+                            <FormControlLabel
+                                value="enseignant"
+                                control={<Radio />}
+                                label="Enseignant"
+                            />
+                        </RadioGroup>
+                    </FormControl>
+
+                    {/* Identifiant */}
+                    <TextField
+                        label="Identifiant"
+                        variant="outlined"
+                        fullWidth
+                        value={identifiant}
+                        onChange={(e) => setIdentifiant(e.target.value)}
+                        required
+                    />
+
+                    {/* Mot de passe */}
+                    <TextField
+                        label="Mot de passe"
+                        variant="outlined"
+                        type="password"
+                        fullWidth
+                        value={motDePasse}
+                        onChange={(e) => setMotDePasse(e.target.value)}
+                        required
+                    />
+
+                    {/* Bouton de connexion (soumission du formulaire) */}
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Connexion
+                    </Button>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
